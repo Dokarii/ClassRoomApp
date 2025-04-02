78e79f5cffd478068138393422243ef1f4450a58
Dokarii <ricardotejedor18@gmail.com>
2025-04-01 21:29:30 -0500

    ManyToOne relations update (OneToMany unfinished)

--- src/main/java/com/example/ClassRoomApp/Models/Student.java
+++ src/main/java/com/example/ClassRoomApp/Models/Student.java
@@ -1,30 +1,55 @@
 package com.example.ClassRoomApp.Models;
 
-import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
 
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "Students")
 public class Student {
-    private Integer id_student;
-    //id_user
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "grade", unique = true, nullable = false)
     private Integer grade;
-    private Date born_day;
+
+    @Column(name = "borndate")
+    private Date borndate;
+
+    @Column(name = "address", length = 255, nullable = false)
     private String address;
+
+    //creando relacion(1 a muchos) con calificaciones
+    @OneToMany(mappedBy = "student")
+    @JsonManagedReference
+    private List<Grade> Grade;
+
+    //creando relacion(1 a muchos) con asistencia
+    @OneToMany(mappedBy = "student")
+    @JsonManagedReference
+    private List<Attendance> attendance;
 
     public Student() {
     }
 
-    public Student(Integer id_student, Integer grade, Date born_day, String address) {
-        this.id_student = id_student;
+    public Student(Integer id, Integer grade, Date borndate, String address) {
+        this.id = id;
         this.grade = grade;
-        this.born_day = born_day;
+        this.borndate = borndate;
         this.address = address;
     }
 
-    public Integer getid_student() {
-        return id_student;
+    public Integer getid() {
+        return id;
     }
 
-    public void setid_student(Integer id_student) {
-        this.id_student = id_student;
+    public void setid(Integer id) {
+        this.id = id;
     }
 
     public Integer getGrade() {
@@ -35,12 +60,12 @@ public class Student {
         this.grade = grade;
     }
 
-    public Date getBorn_day() {
-        return born_day;
+    public Date getborndate() {
+        return borndate;
     }
 
-    public void setBorn_day(Date born_day) {
-        this.born_day = born_day;
+    public void setborndate(Date borndate) {
+        this.borndate = borndate;
     }
 
     public String getAddress() {
