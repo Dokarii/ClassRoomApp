c81044f19dd01c46d00d3d435aa3a4e8c038c0bb
Dokarii <ricardotejedor18@gmail.com>
2025-04-24 08:01:54 -0500

    Interfaces updates & type corrections

---
+++ src/main/java/com/example/ClassRoomApp/Services/ProfessorService.java
@@ -0,0 +1,19 @@
+package com.example.ClassRoomApp.Services;
+
+import com.example.ClassRoomApp.Models.Professor;
+
+import java.util.List;
+
+public class ProfessorService {
+    public List<Professor> ProfessorList() throws Exception {
+    }
+
+    public Professor searchProfessorById() throws Exception {
+        try{
+
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
+
+}
